644391442ee685eb032ea0144f5e1f3dd99f6e48
christophe <christophegueroult@gmail.com>
2021-04-06 15:52:43 +0200

    config scss and start ui component html and scss

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent,
+    IconCloseComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [
+    IconDeleteComponent,
+    IconEditComponent,
+    IconNavComponent,
+    IconCloseComponent,
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
