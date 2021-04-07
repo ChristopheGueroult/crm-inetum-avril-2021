fee44127abe6df67131c10ddf8589e65d89215a3
christophe <christophegueroult@gmail.com>
2021-04-07 08:59:58 +0200

    ngif then else, ng-container and ng-template

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [IconsModule, TemplatesModule, UiModule],
+  exports: [IconsModule, TemplatesModule, UiModule, HeaderComponent, NavComponent, FooterComponent],
 })
 export class CoreModule {}
