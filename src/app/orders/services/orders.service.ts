e40aa82ef7ba8231e8316b85a2abb71510bc401c
christophe <christophegueroult@gmail.com>
2021-04-07 17:47:09 +0200

    concatenation es6 avec environment urlApi

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +10,9 @@ import { Order } from 'src/app/core/models/order';
 export class OrdersService {
   // private collection
   private collection$!: Observable<Order[]>;
+  private urlApi = environment.urlApi;
   constructor(private http: HttpClient) {
-    this.collection = this.http.get<Order[]>('http://localhost:3000/orders');
+    this.collection = this.http.get<Order[]>(`${this.urlApi}/orders`);
   }
 
   // get collection
